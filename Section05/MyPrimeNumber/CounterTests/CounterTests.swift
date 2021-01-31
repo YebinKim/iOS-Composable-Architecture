9d67a0fac0fc935ae6aaa926eb6b2015ec438239
Mumani <yb2316@naver.com>
2021-02-01 01:58:40 +0900

    :memo: Testable State Management: Ergonomics 공부 중
    - Actions sent and actions received

--- Section05/MyPrimeNumber/CounterTests/CounterTests.swift
+++ Section05/MyPrimeNumber/CounterTests/CounterTests.swift
@@ -10,7 +10,7 @@ import XCTest
 import ComposableArchitecture
 
 // MARK: Testable State Management: Ergonomics - The shape of a test
-func assert<Value: Equatable, Action>(
+func assert<Value: Equatable, Action: Equatable>(
     initialValue: Value,
     reducer: Reducer<Value, Action>,
     // MARK: Testable State Management: Ergonomics - Improving test feedback
@@ -20,9 +20,34 @@ func assert<Value: Equatable, Action>(
     line: UInt = #line
 ) {
     var state = initialValue
+    var effects: [Effect<Action>] = []
+
     steps.forEach { step in
         var expected = state
-        _ = reducer(&state, step.action)
+//        _ = reducer(&state, step.action)
+
+        // MARK: Testable State Management: Ergonomics - Actions sent and actions received
+        switch step.type {
+        case .send:
+            effects.append(contentsOf: reducer(&state, step.action))
+
+        case .receive:
+            let effect = effects.removeFirst()
+            var action: Action!
+            let receivedCompletion = XCTestExpectation(description: "receivedCompletion")
+            _ = effect.sink(
+                receiveCompletion: { _ in
+                    receivedCompletion.fulfill()
+                },
+                receiveValue: { action = $0 }
+            )
+            if XCTWaiter.wait(for: [receivedCompletion], timeout: 0.01) != .completed {
+                XCTFail("Timed out waiting for the effect to complete", file: step.file, line: step.line)
+            }
+            XCTAssertEqual(action, step.action, file: step.file, line: step.line)
+            effects.append(contentsOf: reducer(&state, action))
+        }
+
         step.update(&expected)
         XCTAssertEqual(state, expected, file: step.file, line: step.line)
     }
@@ -30,23 +55,32 @@ func assert<Value: Equatable, Action>(
 
 // MARK: Testable State Management: Ergonomics - Improving test feedback
 struct Step<Value, Action> {
+    let type: StepType
     let action: Action
     let update: (inout Value) -> Void
     let file: StaticString
     let line: UInt
 
     init(
+        _ type: StepType,
         _ action: Action,
         file: StaticString = #file,
         line: UInt = #line,
         // MARK: Testable State Management: Ergonomics - Trailing closure ergonomics
         _ update: @escaping (inout Value) -> Void
     ) {
+        self.type = type
         self.action = action
         self.update = update
         self.file = file
         self.line = line
     }
+}
+
+// MARK: Testable State Management: Ergonomics - Actions sent and actions received
+enum StepType {
+  case send
+  case receive
 }
 
 // MARK: Testable State Management: Reducers - Testing the counter
@@ -94,9 +128,9 @@ class CounterTests: XCTestCase {
             initialValue: CounterViewState(count: 2),
             reducer: counterViewReducer,
             steps:
-            Step(.counter(.increaseCount), { $0.count = 3 }),
-            Step(.counter(.increaseCount), { $0.count = 4 }),
-            Step(.counter(.decreaseCount), { $0.count = 3 })
+            Step(.send, .counter(.increaseCount), { $0.count = 3 }),
+            Step(.send, .counter(.increaseCount), { $0.count = 4 }),
+            Step(.send, .counter(.decreaseCount), { $0.count = 3 })
         )
     }
 
@@ -113,47 +147,67 @@ class CounterTests: XCTestCase {
 
     // MARK: Testable State Management: Reducers - Unhappy paths and integration tests
     func testNthPrimeButtonHappyFlow() {
+
+        // MARK: Testable State Management: Ergonomics - Actions sent and actions received
         Current.nthPrime = { _ in .sync { 17 } }
 
-        var state = CounterViewState(
-            alertNthPrime: nil,
-            isNthPrimeButtonDisabled: false
-        )
-        var expected = state
-        var effects = counterViewReducer(&state,
-                                         .counter(.nthPrimeButtonTapped))
-
-        expected.isNthPrimeButtonDisabled = true
-        XCTAssertEqual(state, expected)
-        XCTAssertEqual(effects.count, 1)
-
-        var nextAction: CounterViewAction!
-        let receivedCompletion = self.expectation(description: "receivedCompletion")
-        let cancellable = effects[0].sink(
-            receiveCompletion: { _ in
-                receivedCompletion.fulfill()
+        assert(
+            initialValue: CounterViewState(
+                alertNthPrime: nil,
+                isNthPrimeButtonDisabled: false
+            ),
+            reducer: counterViewReducer,
+            steps:
+            Step(.send, .counter(.nthPrimeButtonTapped)) {
+                $0.isNthPrimeButtonDisabled = true
             },
-            receiveValue: { action in
-                XCTAssertEqual(action, .counter(.nthPrimeResponse(17)))
-                nextAction = action
+            Step(.receive, .counter(.nthPrimeResponse(17))) {
+                $0.alertNthPrime = PrimeAlert(prime: 17)
+                $0.isNthPrimeButtonDisabled = false
+            },
+            Step(.send, .counter(.alertDismissButtonTapped)) {
+                $0.alertNthPrime = nil
             }
         )
-        self.wait(for: [receivedCompletion], timeout: 0.01)
-
-        effects = counterViewReducer(&state,
-                                     nextAction)
-
-        expected.alertNthPrime = PrimeAlert(prime: 17)
-        expected.isNthPrimeButtonDisabled = false
-        XCTAssertEqual(state, expected)
-        XCTAssertTrue(effects.isEmpty)
-
-        effects = counterViewReducer(&state,
-                                     .counter(.alertDismissButtonTapped))
-
-        expected.alertNthPrime = nil
-        XCTAssertEqual(state, expected)
-        XCTAssertTrue(effects.isEmpty)
+//        var state = CounterViewState(
+//            alertNthPrime: nil,
+//            isNthPrimeButtonDisabled: false
+//        )
+//        var expected = state
+//        var effects = counterViewReducer(&state,
+//                                         .counter(.nthPrimeButtonTapped))
+//
+//        expected.isNthPrimeButtonDisabled = true
+//        XCTAssertEqual(state, expected)
+//        XCTAssertEqual(effects.count, 1)
+//
+//        var nextAction: CounterViewAction!
+//        let receivedCompletion = self.expectation(description: "receivedCompletion")
+//        let cancellable = effects[0].sink(
+//            receiveCompletion: { _ in
+//                receivedCompletion.fulfill()
+//            },
+//            receiveValue: { action in
+//                XCTAssertEqual(action, .counter(.nthPrimeResponse(17)))
+//                nextAction = action
+//            }
+//        )
+//        self.wait(for: [receivedCompletion], timeout: 0.01)
+//
+//        effects = counterViewReducer(&state,
+//                                     nextAction)
+//
+//        expected.alertNthPrime = PrimeAlert(prime: 17)
+//        expected.isNthPrimeButtonDisabled = false
+//        XCTAssertEqual(state, expected)
+//        XCTAssertTrue(effects.isEmpty)
+//
+//        effects = counterViewReducer(&state,
+//                                     .counter(.alertDismissButtonTapped))
+//
+//        expected.alertNthPrime = nil
+//        XCTAssertEqual(state, expected)
+//        XCTAssertTrue(effects.isEmpty)
     }
 
     func testNthPrimeButtonUnhappyFlow() {
